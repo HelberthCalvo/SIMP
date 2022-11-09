05b73d026381676bcd9be40fecb0d99bce194958
hcalvo <hcalvo@sitsacr.net>
2022-11-08 18:48:32 -0600

    Envio de reporte correo

--- SIMP.Logica/UTILITIES/PrintHelper.cs
+++ SIMP.Logica/UTILITIES/PrintHelper.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing.Printing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,11 +43,7 @@ namespace SIMP.Logica.UTILITIES
           reportDocument.Load(reportDocument.FileName);
           reportDocument.Refresh();
 
-
-
           ICollection listaNombresParametros = parametrosReporte.Keys;
-
-
 
           foreach (string nombreParametro in listaNombresParametros)
           {
@@ -60,20 +57,66 @@ namespace SIMP.Logica.UTILITIES
 
           //int fin = reportDocument.FormatEngine.GetLastPageNumber(new CrystalDecisions.Shared.ReportPageRequestContext());
           reportDocument.PrintToPrinter(printerSettings, new PageSettings(), false);
+          
           //reportDocument.PrintToPrinter(1, false, 0, 0);
-
-
-
         }
-
-
 
       }
       catch (Exception ex)
       {
+        throw new Exception("Ocurrio un error al intentar imprimir el tiquete. Verifique que la impresora esté conectada. " + ex.Message);
+      }
+    }
+
+    public static Stream generarPDF(Hashtable parametrosReporte, string nombreImpresora, string rutaReporte)
+    {
+      try
+      {
+        string var_reporte = rutaReporte;
+        TableLogOnInfos crtableLogoninfos = new TableLogOnInfos();
+        TableLogOnInfo crtableLogoninfo = new TableLogOnInfo();
+        ConnectionInfo crConnectionInfo = new ConnectionInfo();
+        Conexion con = new Conexion();
+        crConnectionInfo.UserID = con.ObtenerDato(2);  //"ECOMMERCE"; 
+        crConnectionInfo.Password = con.ObtenerDato(3); //"Sa123";
+        crConnectionInfo.DatabaseName = con.ObtenerDato(1);  //"Exactus";
+        crConnectionInfo.ServerName = con.ObtenerDato(0); // "Exactus";
+
+        //  Conexion con = new Conexion();
+        //  conex.DatabaseName = con.ObtenerDato(1);  //"Exactus";
+        //  conex.UserID = con.ObtenerDato(2);  //"ECOMMERCE";  
+        //  conex.Password = con.ObtenerDato(3); //"Sa123";
+        //  conex.ServerName = con.ObtenerDato(0); // "Exactus";
+        //  conex.Type = ConnectionInfoType.SQL;
 
 
+        using (ReportDocument reportDocument = new ReportDocument())
+        {
+          reportDocument.FileName = var_reporte;
+          reportDocument.Load(reportDocument.FileName);
+          reportDocument.Refresh();
 
+          ICollection listaNombresParametros = parametrosReporte.Keys;
+
+          foreach (string nombreParametro in listaNombresParametros)
+          {
+            reportDocument.SetParameterValue(nombreParametro, parametrosReporte[nombreParametro]);
+          }
+          //System.Drawing.Printing.PrinterSettings printerSettings = new System.Drawing.Printing.PrinterSettings();
+          PrinterSettings printerSettings = new PrinterSettings();
+          printerSettings.PrinterName = nombreImpresora;
+
+          reportDocument.SetDatabaseLogon(crConnectionInfo.UserID, crConnectionInfo.Password);
+
+          //int fin = reportDocument.FormatEngine.GetLastPageNumber(new CrystalDecisions.Shared.ReportPageRequestContext());
+          return reportDocument.ExportToStream(ExportFormatType.PortableDocFormat);
+
+          //reportDocument.PrintToPrinter(1, false, 0, 0);
+        }
+
+      }
+      catch (Exception ex)
+      {
         throw new Exception("Ocurrio un error al intentar imprimir el tiquete. Verifique que la impresora esté conectada. " + ex.Message);
       }
     }
--- SIMP/Reporte.aspx
+++ SIMP/Reporte.aspx
@@ -51,10 +51,10 @@
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Progreso Proyecto</button>
                 </li>
-                <li class="nav-item" role="presentation">
+                <li class="nav-item" role="presentation" style="display: none">
                     <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Tiempos</button>
                 </li>
-                <li class="nav-item" role="presentation">
+                <li class="nav-item" role="presentation" style="display: none">
                     <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Carga Trabajo</button>
                 </li>
 
